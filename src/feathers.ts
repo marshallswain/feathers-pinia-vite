5d8c9c67119395dd44a45c9aa18c7471745458e8
Marshall Thompson <marshall@creativeideal.net>
2023-01-02 11:13:23 -0700

    feat: feathers-dove types installed from api

--- src/feathers.ts
+++ src/feathers.ts
@@ -1,10 +1,17 @@
-import { feathers } from '@feathersjs/feathers'
+import { createClient } from 'feathers-pinia-api'
 import socketio from '@feathersjs/socketio-client'
-import auth from '@feathersjs/authentication-client'
 import io from 'socket.io-client'
-
-import { createClient } from 'feathers-pinia-nuxt3-api'
+import { useStorage } from '@vueuse/core'
 
 const socket = io('http://localhost:3030', { transports: ['websocket'] })
 
-export const api = createClient(socketio(socket), { storage: window.localStorage })
+// Store JWT in SSG-compatible storage
+const storageKey = 'feathers-jwt'
+const jwt = useStorage(storageKey, '')
+const storage = {
+  getItem: () => jwt.value,
+  setItem: (val: string) => jwt.value = val,
+  removeItem: () => jwt.value = null,
+}
+
+export const api = createClient(socketio(socket), { storage })
