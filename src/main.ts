6c729d960e133e65dc7e10c8bde348fa27ce00f2
Marshall Thompson <marshall@creativeideal.net>
2023-01-02 11:13:35 -0700

    fix: pinia location in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { pinia } from './store/store.pinia'
+import { pinia } from './modules/pinia'
 import { router } from './router'
 import { createHead } from '@vueuse/head'
 
