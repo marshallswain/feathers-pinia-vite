60d4927d99fc00779d2b2f0136dd4880e94a319a
Marshall Thompson <marshall@creativeideal.net>
2023-01-03 00:51:15 -0700

    refactor: lint cleanup

--- src/composables/use-feathers.ts
+++ src/composables/use-feathers.ts
@@ -10,7 +10,10 @@ export const useFeathers = () => {
  * Returns a type-casted service to work with Feathers-Pinia. It currently does not type custom methods.
  * @param servicePath the path of the service
  */
-export const useFeathersService = <Result extends AnyData, Query extends AnyData>(servicePath: string, clientAlias = 'api') => {
+export const useFeathersService = <Result extends AnyData, Query extends AnyData>(
+  servicePath: string,
+  clientAlias = 'api',
+) => {
   const clients = useFeathers()
   const client = clients[clientAlias as keyof typeof clients]
   return client.service(servicePath as any) as unknown as FeathersPiniaService<Result, Query>
